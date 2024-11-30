172c052bcc1fb9a91a9897e4dd0c6ee28b630b14
MohamedTaha <medotaha804@gmail.com>
2024-11-30 17:45:20 +0200

    Adding Entities,DbContext,Migrations

---
+++ Dating.API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Dating.Data.Entities;
+using Dating.Repository.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DatingDbContext context) : ControllerBase
+{
+
+    [HttpGet] // GET : /api/Users
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id:int}")] // GET : /api/Users/1
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        return user is null ? NotFound() : Ok(user);
+    }
+    
+}
---
+++ Dating.Repository/Data/DatingDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Dating.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating.Repository.Data;
+
+public class DatingDbContext(DbContextOptions<DatingDbContext>options):DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
